9a94e986b15b7899186adabb14e62a01c123551f
AlienInvasion <vijayvsph@gmail.com>
2025-09-19 17:53:28 +0530

    Added some views and choices.

--- djangotutorial/polls/urls.py
+++ djangotutorial/polls/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='Index'),
+    path('<int:question_id>/detail/', views.detail, name = 'Detail'),
+    path('<int:question_id>/result/', views.results, name = 'Result'),
+    path('<int:question_id>/vote/', views.vote, name = "Vote"),
+    
 ]
--- djangotutorial/polls/views.py
+++ djangotutorial/polls/views.py
@@ -1,6 +1,21 @@
 #from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Hello World. You\'re at the polls index.')
+    #return HttpResponse('Hello World. You\'re at the polls index.')
+    recent_questions = Question.objects.order_by('-pub_date')[:5]
+    res = [q.question_text for q in recent_questions]
+    output = ', '.join(res)
+    return HttpResponse(output)
+
+def detail(request, question_id):
+    return HttpResponse('You\'re looking at the question {}.'.format(question_id))
+
+def results(request, question_id):
+    response = f'You\'re looking at the results of the question {question_id}'
+    return HttpResponse(response)
+
+def vote(request, question_id):
+    return HttpResponse('Your voting on the question %s.' % question_id)
